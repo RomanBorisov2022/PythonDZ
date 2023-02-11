aa5f31f8abcb81ba4f9e2d1904c79af8f5cf5c94
Roman Borisov <boriso-rom@yandex.ru>
2023-02-11 12:13:59 +0300

    added code to task 2 in DZ 5

--- DZ_05/Task_2.py
+++ DZ_05/Task_2.py
@@ -3,3 +3,15 @@
 
 # 2 2
 # 4
+
+n = int(input('Введите первое число: '))
+m = int(input('Введите второе число: '))
+
+def sum_numbers(n, m):
+    # sum = None
+    for i in range(1, n+1):
+        sum = n+m
+    return sum
+
+a = sum_numbers(n, m)
+print(a)
