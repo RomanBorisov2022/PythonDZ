aadb8c4b846fa638c79145dfaf358c24008ca179
Roman Borisov <boriso-rom@yandex.ru>
2023-02-11 12:11:34 +0300

    added code to task 1 in DZ 5

--- DZ_05/task_1.py
+++ DZ_05/task_1.py
@@ -3,3 +3,14 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
 
+n = int(input('Введите первое число: '))
+m = int(input('Введите второе число: '))
+
+def expon_numbers(n, m):
+    expon = None
+    for i in range(1, n+1):
+        expon = n**m
+    return expon
+
+a = expon_numbers(n, m)
+print(a)
