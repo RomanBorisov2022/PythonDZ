92eaad73ce0680ba337ad5a422646d35922a5910
Roman Borisov <boriso-rom@yandex.ru>
2023-02-18 09:57:41 +0300

    added code to task 3 in DZ 6

--- DZ_06/Task_2.py
+++ DZ_06/Task_2.py
@@ -2,3 +2,19 @@
 # значения которых принадлежат заданному диапазону 
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
+from random import randint
+# n = []
+# a = int(input('Введите колличество элементов массива: '))
+# for i in range(a):
+#     a = random.randint(1, 10)
+#     n.append(a)
+# print(*n)
+
+arr = [randint(1, 10) for i in range(int(input('Введите N: ')))]
+count = 0
+print(*arr)
+
+for i in range(len(arr)):
+    if arr[i - 1] < arr[i] > arr[(i + 1) % len(arr)]:
+        count += 1
+        print(i)
--- DZ_06/Task_3.py
+++ DZ_06/Task_3.py
@@ -8,3 +8,15 @@
 # Пары необходимо выводить по одной в строке, разделяя пробелами. 
 # Каждая пара должна быть выведена только один раз (перестановка чисел новую пару не дает).
 
+n = int(input())
+list_1 = list()
+for i in range(n):
+    summa = 0
+    for j in range(1, i // 2 + 1):
+        if i % j == 0:
+            summa += j
+    list_1.append(tuple([i, summa]))
+for i in range(len(list_1)):
+    for j in range(i, len(list_1)):
+        if i != j and list_1[i][0] == list_1[j][1] and list_1[i][1] == list_1[j][0]:
+            print(*list_1[i])
